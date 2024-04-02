b4dd26e4e23dee4ae9cb8a03c96b93f783b6ef64
Alexander Vetlugin <vetlugins@gmail.com>
2024-04-02 14:36:53 +0300

    1.0.8

--- src/VGRollup/js/VGRollup.js
+++ src/VGRollup/js/VGRollup.js
@@ -8,7 +8,7 @@ class VGRollup {
 						button: "vg-rollup-content--button",
 						transition: "vg-rollup-content--transition"
 				};
-				
+
 				this.settings = mergeDeepObject({
 						content: 'text',
 						fade: true,
@@ -26,25 +26,25 @@ class VGRollup {
 				if (this.target) {
 						let $element = document.getElementById(this.target);
 						this.init($element);
-						
+
 						let $toggle = document.querySelector("[data-rollup-toggle=\"" + this.target + "\"]");
 						if ($toggle) this.toggle($toggle, callback);
 				} else {
 						console.error("Initialization failed");
 				}
 		}
-		
+
 		init($self, isButtonAppend = true) {
 				let _this = this,
 						self_height = $self.clientHeight, set_height = parseInt($self.dataset.height) || (self_height / 2);
-				
+
 				let isFade = $self.dataset.fade !== "false" && _this.settings.fade,
 					isTransition = $self.dataset.transition === "true" || _this.settings.transition,
 					isEllipsis = $self.dataset.line || _this.settings.ellipsis.line !== null,
 					isButton = $self.dataset.button !== "false" && _this.settings.button;
-				
+
 				if (!isButtonAppend) _this.switch($self);
-				
+
 				if (self_height > set_height && _this.settings.content === 'text') {
 						$self.classList.add(_this.classes.hidden);
 						$self.style.height = set_height + "px";
@@ -53,18 +53,18 @@ class VGRollup {
 						transition();
 						fade();
 						button();
-				} else if(_this.settings.content === 'elements') {
+				} else if (_this.settings.content === 'elements') {
 						let elementClass = $self.dataset.elements || 'item',
 								items = $self.querySelectorAll('.' + elementClass),
 								cnt = Number($self.dataset.cnt) || 5,
 								i = 0;
-						
+
 						for (const item of items) {
 								if (!item.classList.contains('show-more')) {
 										if (i > cnt) {
 												item.classList.add('d-none')
 										}
-										
+
 										i++;
 								}
 						}
@@ -74,39 +74,39 @@ class VGRollup {
 						fade();
 						button();
 				}
-				
+
 				function ellipsis() {
 						if (isEllipsis) {
 								let line = $self.dataset.line || _this.settings.ellipsis.line;
 								isFade = false;
-								
+
 								if (line) {
 										$self.classList.add(_this.classes.ellipsis);
-										
+
 										$self.style.webkitLineClamp = line;
 								} else {
 										console.log("The [data-line] or param[line] variable should not be empty");
 								}
 						}
 				}
-				
+
 				function transition() {
 						if (isTransition) {
 								// TODO no work
 								$self.classList.add(_this.classes.transition);
 						}
 				}
-				
+
 				function fade() {
 						if (isFade) {
 								$self.classList.add(_this.classes.fade);
 						}
 				}
-				
+
 				function button() {
 						if (isButtonAppend) {
 								$self.setAttribute("id", _this.target);
-								
+
 								if (isButton) {
 										let btnTextMore = $self.dataset.buttonMore || _this.settings.buttonText.more;
 										$self.insertAdjacentHTML("afterend", "<div  class=\"" + _this.classes.button + "\"><a href=\"#\" aria-expanded=\"false\" data-rollup-toggle=\"" + _this.target + "\">" + btnTextMore + "</a></div>");
@@ -114,19 +114,19 @@ class VGRollup {
 						}
 				}
 		}
-		
+
 		switch($self, switcher = false) {
 				const _this = this;
-				
+
 				if (switcher) {
 						this.init($self, false);
 				} else {
 						$self.classList.remove(this.classes.hidden);
 						$self.classList.remove(this.classes.ellipsis);
 						$self.classList.remove(this.classes.fade);
-						
+
 						$self.removeAttribute("style");
-						
+
 						if (_this.settings.content === 'elements') {
 							let items = [...$self.querySelectorAll('.d-none')];
 							if (items.length) {
@@ -135,31 +135,31 @@ class VGRollup {
 						}
 				}
 		}
-		
+
 		toggle($toggle, callback) {
 				let $_this = this;
-				
+
 				$toggle.onclick = function(e) {
 						let $btn = e.target, target = $btn.dataset.rollupToggle, aria = $btn.getAttribute("aria-expanded");
-						
+
 						let $target = document.getElementById(target);
-						
+
 						if (aria === "false") {
 								$btn.setAttribute("aria-expanded", true);
 								$btn.text = $target.dataset.buttonLess || $_this.settings.buttonText.less;
-								
+
 								$_this.switch($target, false);
-								
+
 								// TODO callback the area is show
 						} else {
 								$btn.setAttribute("aria-expanded", false);
 								$btn.text = $target.dataset.buttonMore || $_this.settings.buttonText.more;
-								
+
 								$_this.switch($target, true);
-								
+
 								// TODO callback the area is hide
 						}
-						
+
 						return false;
 				};
 		}
@@ -172,12 +172,12 @@ class VGRollup {
  */
 function mergeDeepObject(...objects) {
 		const isObject = obj => obj && typeof obj === "object";
-		
+
 		return objects.reduce((prev, obj) => {
 				Object.keys(obj).forEach(key => {
 						const pVal = prev[key];
 						const oVal = obj[key];
-						
+
 						if (Array.isArray(pVal) && Array.isArray(oVal)) {
 								prev[key] = pVal.concat(...oVal);
 						} else if (isObject(pVal) && isObject(oVal)) {
@@ -186,7 +186,7 @@ function mergeDeepObject(...objects) {
 								prev[key] = oVal;
 						}
 				});
-				
+
 				return prev;
 		}, {});
 }
